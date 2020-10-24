aba16b5fda1c80672254f330119ddba082f4ac90
Kelsi <kelsiproulx1919@gmail.com>
2020-10-23 20:11:30 -0700

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import Error from './components/Error';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: 'Click the button to load data!'
-    };
-  }
-
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        console.log(response.data); // The entire response from the Rails API
-
-        console.log(response.data.message); // Just the message
-        this.setState({
-          message: response.data.message
-        });
-      });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h1>{this.state.message}</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>
-      </div>
-    );
-  }
+function App() {
+  <Router>
+    <Switch>
+      <Route path='/' component={Home} exact />
+      <Route component={Error} />
+    </Switch>
+  </Router>;
 }
 
 export default App;
---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css';
+
+function Error() {
+  <h1>Oops! Page not found!</h1>;
+}
+
+export default Error;
