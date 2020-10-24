73b59b357b5eb0d68827055789d6a3cc952b0eff
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-10-24 00:55:10 +0000

    remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       message: 'Click the button to load data!'
-    }
+    };
   }
 
   fetchData = () => {
     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+      .then((response) => {
+        // handle success
+        console.log(response.data); // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
+        console.log(response.data.message); // Just the message
+        this.setState({
+          message: response.data.message
+        });
       });
-    }) 
-  }
+  };
 
   render() {
     return (
       <div className="App">
-        <h1>{ this.state.message }</h1>
+        <h1>{this.state.message}</h1>
         <button onClick={this.fetchData} >
           Fetch Data
-        </button>        
+        </button>
       </div>
     );
   }
