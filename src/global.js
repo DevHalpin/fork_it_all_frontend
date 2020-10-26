dd7a2286eeab8c308ce3841d71b812f429a3460a
Kelsi <kelsiproulx1919@gmail.com>
2020-10-26 14:39:03 -0700

    add hamburger menu

---
+++ src/global.js
@@ -0,0 +1,23 @@
+// Using styles from styled-components library, can be used throughout app
+
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+  body {
+    align-items: center;
+    background: ${({theme}) => theme.primaryDark};
+    color: ${({theme}) => theme.primaryLight};
+    display: flex;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    height: 100vh;
+    justify-content: center;
+    text-rendering: optimizeLegibility;
+  }
+  `;
