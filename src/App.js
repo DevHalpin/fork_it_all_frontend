8190783cfb6aad289d1f7b5596595456733a86be
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-26 11:52:49 -0700

    delete project within a project

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import Recipes from "./components/Recipes";
 import My_Twists from "./components/My_Twists";
 import Fave_Twists from "./components/Fave_Twists";
 import Fave_Users from "./components/Fave_Users";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import User_Dashboard from "./components/User_Dashboard";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
@@ -19,10 +20,11 @@ export default function App() {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/login" component={Login} />
-        <Route path="/users" component={User_Profile} />
+        <Route path="/user_profile" component={User_Profile} />
         <Route path="/recipes" component={Recipes} />
         <Route path="/my_twists" component={My_Twists} />
         <Route path="/fave_twists" component={Fave_Twists} />
+        <Route path="/user_dashboard" component={User_Dashboard} />
         <Route path="/fave_users" component={Fave_Users} />
         <Route component={Error} />
       </Switch>
