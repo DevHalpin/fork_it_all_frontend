693f5d3ff4135cba674ff5503341ce57fa6c1c3e
DevHalpin <echidnablade@hotmail.com>
2020-10-26 14:01:21 -0700

    Add Landing Page and Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import NavbarNav from "./components/Nav";
+import Footer from "./components/Footer"
 import Home from "./components/Home";
 import Error from "./components/Error";
 import Login from "./components/Login";
@@ -26,6 +27,7 @@ export default function App() {
         <Route path="/fave_users" component={Fave_Users} />
         <Route component={Error} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,54 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import "bootstrap/dist/css/bootstrap.min.css";
-import '../App.scss';
+  import React, { useState } from 'react';
+  import axios from 'axios';
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import '../App.scss';
+  
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: 'Click the button to load data!'
-    };
-  };
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        console.log(response.data); // The entire response from the Rails API
+  export default function Home() {
+      const [message, setMessage] = useState("Click to fetch data!")
 
-        console.log(response.data.message); // Just the message
-        this.setState({
-          message: response.data.message
+    const fetchData = () => {
+      axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+        .then((response) => {
+          // handle success
+          console.log(response.data); // The entire response from the Rails API
+
+          console.log(response.data.message); // Just the message
+          setMessage(
+            response.data.message
+          );
         });
-      });
-  };
+    };
 
-  render() {
-    return (
-      <div>
-        <h1>{this.state.message}</h1>
-        <button onClick={this.fetchData} >
+      return (
+        
+      <div class="container mw-100">    
+       <div>
+        <h1>{message}</h1>
+        <button onClick={fetchData} >
           Fetch Data
         </button>
       </div>
-    );
+        <div class="row">
+          <div class="col-sm-6 text-center">
+            <img src="http://www.fillmurray.com/500/500" alt=""></img>
+          </div>
+          <div class="col-sm-6">
+            <h2>Heading</h2>
+            <p>This is a bunch of text that is repeated a lot.This is a bunch of text that is repeated a lot.This is a bunch of text that is repeated a lot.This is a bunch of text that is repeated a lot.This is a bunch of text that is repeated a lot.This is a bunch of text that is repeated a lot.This is a bunch of text that is repeated a lot.This is a bunch of text that is repeated a lot.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="bg-success col-sm-6 text-center">
+            <p>Fork It Favorites</p>
+          <img src="http://www.fillmurray.com/500/500" alt=""></img>
+          </div>
+          <div class="bg-info col-sm-6 text-center">
+            <p>Random Twists</p>
+          <img src="http://www.fillmurray.com/500/500" alt=""></img>
+          </div>
+        </div>
+      </div>
+      );
   }
-}
-
-export default Home;
