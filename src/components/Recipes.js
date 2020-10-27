49080eb07fc018ad444d2ee0ee4408cf28d16dbe
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-27 08:41:35 -0700

    got permalinks working for recipes

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,18 +1,32 @@
-import React, {useState, useEffect} from "react";
-import {Dropdown, DropdownButton, Card, CardDeck, Container, Col, Button, Form} from 'react-bootstrap';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  Dropdown,
+  DropdownButton,
+  Card,
+  CardDeck,
+  Container,
+  Col,
+  Button,
+  Form,
+} from "react-bootstrap";
+import axios from "axios";
 
-const Recipes = () => {
+const Recipes = (props) => {
+  const id = props.match.params.recipe;
+  console.log(props.match.params.recipe);
   const fakeRecipe = {
     id: 2,
     name: "Mini burgers",
     region: "American",
-    instructions: "1. Preheat the oven to 250 degrees F. Preheat a griddle to 350 degrees F. 2. Combine the onion powder, garlic powder, pepper, and salt in a small bowl.Set aside. 3. Line a jellyroll or sheet pan with parchment paper, and place the ground chuck in the middle of the pan.Cover the meat with a large sheet of plastic wrap.Roll meat with a rolling pin until it covers the surface of the pan; it should be very thin.Remove the plastic wrap, and sprinkle the meat with the seasoning mixture.Fold the meat in half, from side to side, using the parchment paper.Use a pizza wheel to cut the meat into 8 even squares. 4. Wrap the buns in foil and place in the oven for 5 to 10 minutes.Meanwhile, place the burgers on the griddle and cook for 2 to 3 minutes per side. 5. Remove the buns from the oven.Spread a small amount of mayonnaise on each bun and top with the burger and any other condiments, as desired.Serve immediately.",
-    meal_image: "https://pizzazzerie.com/wp-content/uploads/2015/08/party-food-mini-cheeseburgers-pizzazzerie.jpg",
+    instructions:
+      "1. Preheat the oven to 250 degrees F. Preheat a griddle to 350 degrees F. 2. Combine the onion powder, garlic powder, pepper, and salt in a small bowl.Set aside. 3. Line a jellyroll or sheet pan with parchment paper, and place the ground chuck in the middle of the pan.Cover the meat with a large sheet of plastic wrap.Roll meat with a rolling pin until it covers the surface of the pan; it should be very thin.Remove the plastic wrap, and sprinkle the meat with the seasoning mixture.Fold the meat in half, from side to side, using the parchment paper.Use a pizza wheel to cut the meat into 8 even squares. 4. Wrap the buns in foil and place in the oven for 5 to 10 minutes.Meanwhile, place the burgers on the griddle and cook for 2 to 3 minutes per side. 5. Remove the buns from the oven.Spread a small amount of mayonnaise on each bun and top with the burger and any other condiments, as desired.Serve immediately.",
+    meal_image:
+      "https://pizzazzerie.com/wp-content/uploads/2015/08/party-food-mini-cheeseburgers-pizzazzerie.jpg",
     meal_type: "Burgers",
     video_url: null,
     created_at: "2020-10-26 23:14:20.411925",
-    updated_at: "2020-10-26 23:14:20.411925"
+    updated_at: "2020-10-26 23:14:20.411925",
   };
   const fakeTwist = {
     id: 101,
@@ -22,9 +36,10 @@ const Recipes = () => {
     slug: "in_voluptatem",
     is_private: false,
     sort_order: 101,
-    content: "Use the digital TCP feed, then you can back up the bluetooth alarm!",
+    content:
+      "Use the digital TCP feed, then you can back up the bluetooth alarm!",
     created_at: "2020-10-26 23:14:20.411925",
-    updated_at: "2020-10-26 23:14:20.411925"
+    updated_at: "2020-10-26 23:14:20.411925",
   };
   const fakeUser = {
     id: 101,
@@ -39,17 +54,18 @@ const Recipes = () => {
   };
 
   const [recipe, setRecipe] = useState(fakeRecipe);
+
   // Make a request for a user with a given ID
   useEffect(() => {
     axios
-      .get("/api/recipes/1")
+      .get(`/api/recipes/${id}`)
       .then((response) => {
         console.log(response.data);
         // let parsedResponse = JSON.parse(response.data.message);
         // console.log(parsedResponse);
         setRecipe(response.data);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }, []);
@@ -58,14 +74,14 @@ const Recipes = () => {
   // Make a request for a user with a given ID
   useEffect(() => {
     axios
-      .get("/api/twists/10")
+      .get("/api/twists/18")
       .then((response) => {
         console.log(response.data);
         // let parsedResponse = JSON.parse(response.data.message);
         // console.log(parsedResponse);
         setTwist(response.data);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }, []);
@@ -74,14 +90,14 @@ const Recipes = () => {
   // Make a request for a user with a given ID
   useEffect(() => {
     axios
-      .get("/api/users/100")
+      .get("/api/users/95")
       .then((response) => {
         console.log(response.data);
         // let parsedResponse = JSON.parse(response.data.message);
         // console.log(parsedResponse);
         setUser(response.data);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }, []);
@@ -91,7 +107,11 @@ const Recipes = () => {
     <>
       <Container>
         <Col>
-          < DropdownButton title="Recipe Options" align="right" className="recipe-dropdown">
+          <DropdownButton
+            title="Recipe Options"
+            align="right"
+            className="recipe-dropdown"
+          >
             <Dropdown.Item href="#/action-1">Share</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Rate</Dropdown.Item>
             <Dropdown.Item href="#/action-3">Create Twist</Dropdown.Item>
@@ -101,10 +121,12 @@ const Recipes = () => {
 
         {/* // Recipe display */}
         <CardDeck className="recipe-columns">
-          <Card className="recipe-card" >
+          <Card className="recipe-card">
             <Card.Img src={`${recipe.meal_image}`} />
             <Card.Body className="recipe-body">
-              <Card.Header as="h5" className="text-center">{`${recipe.name}`} </Card.Header>
+              <Card.Header as="h5" className="text-center">
+                {`${recipe.name}`}{" "}
+              </Card.Header>
               <Card.Text className="recipe-text">
                 {`${recipe.instructions}`}
               </Card.Text>
@@ -114,22 +136,25 @@ const Recipes = () => {
           <Card className="text-center twist-card">
             <Card.Header as="h5">User Twists!</Card.Header>
             <Card.Body>
-              <Card.Title>{user.handle} suggests including the following twist:</Card.Title>
-              <Card.Text>
-                {twist.content}
-              </Card.Text>
+              <Card.Title>
+                {user.handle} suggests including the following twist:
+              </Card.Title>
+              <Card.Text>{twist.content}</Card.Text>
               <Button variant="primary">Find a random Twist</Button>
             </Card.Body>
             <Form>
               <Form.Group as={Col}>
                 <Form.Label>Find Twists by User</Form.Label>
-                <Form.Control size="md" type="text" placeholder="Enter a user handle" />
+                <Form.Control
+                  size="md"
+                  type="text"
+                  placeholder="Enter a user handle"
+                />
               </Form.Group>
 
               <Form.Group as={Col}>
                 <Form.Label>Search by Twist Type</Form.Label>
-                <Form.Control as="select" id="inlineFormCustomSelect"
-                  custom>
+                <Form.Control as="select" id="inlineFormCustomSelect" custom>
                   <option value="0">Select an option</option>
                   <option value="1">Ingredient Replacement</option>
                   <option value="2">Cooking Time</option>
