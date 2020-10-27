eecf0f1c8df217a9cf6aa6f08dce205e5ea02ab2
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-27 13:12:20 -0700

    attempted to make login work its kind of stateful but it doesnt' work yet there are some state bugs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/Login.scss";
+import axios from "axios";
 
 function Login() {
+  function handleChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function loginEvent(event) {
+    event.preventDefault();
+    console.log(event);
+    axios
+      .get("/api/users/1")
+      .then((response) => {
+        console.log(response.data);
+        // let parsedResponse = JSON.parse(response.data.message);
+        // console.log(parsedResponse);
+        setUsername(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  const [username, setUsername] = useState("");
   return (
     <div className="container login-container">
       <div className="row">
         <div className="col-md-6 login-form-1">
           <h3>Login</h3>
-          <form>
+          <form className="form-control" onSubmit={loginEvent}>
             <div className="form-group">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Username*"
-                value=""
+                value={username}
+                onChange={handleChange}
               />
             </div>
             <div className="form-group">
@@ -22,6 +45,7 @@ function Login() {
                 type="password"
                 className="form-control"
                 placeholder="Your Password *"
+                onChange={handleChange}
                 value=""
               />
             </div>
@@ -58,7 +82,9 @@ function Login() {
               <input type="submit" className="btnSubmit" value="Login" />
             </div>
             <div className="form-group">
-              <button type="button" class="mr-sm-2 btn btn-primary">Forgot password?</button>
+              <button type="button" class="mr-sm-2 btn btn-primary">
+                Forgot password?
+              </button>
             </div>
           </form>
         </div>
