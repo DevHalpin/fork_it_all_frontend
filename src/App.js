716ce63713b9467b45971e521045cfd4af20dd8a
Kelsi <kelsiproulx1919@gmail.com>
2020-10-27 11:02:24 -0700

    add fave twists and fave users pages

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ export default function App() {
         <Route exact path="/recipes/:recipe/twists/:twist" component={Recipes} />
         <Route exact path="/recipes/:recipe" component={Recipes} />
         <Route exact path="/my_twists/:recipe/twists/:twist" component={My_Twists} />
+        <Route exact path="/fave_twists/:recipe/twists/:twist" component={Fave_Twists} />
         <Route path="/recipes" component={Recipes} />
         <Route path="/my_twists" component={My_Twists} />
         <Route path="/fave_twists" component={Fave_Twists} />
