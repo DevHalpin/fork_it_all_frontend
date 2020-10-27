f424acfd74213b857266fdd58ac6f9f6691e5737
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-26 23:18:28 -0700

    fix bug where useState would error the home page out - An initial state MUST be set to avoid this bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import '../App.scss';
-import {Container, Carousel, Row, Col, Image} from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../styles/App.scss";
+import { Container, Carousel, Row, Col, Image } from "react-bootstrap";
 
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
@@ -46,55 +46,64 @@ function ControlledCarousel() {
           <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
 }
 
+export default function Home() {
+  // const [message, setMessage] = useState("Click to fetch data!")
+  // a value has to be put in as an inisial state or the app breaks!!
+  const [data, setData] = useState(0);
+  const [recipe, setRecipe] = useState(0);
 
-<<<<<<< HEAD
-useEffect(() => {
-  axios
-    .get("/api/twists/1")
-    .then((result) => {
-      setData(result.data);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-}, []);
-=======
-  export default function Home() {
-    const [data, setData] = useState({id:1000});
->>>>>>> 6f81b2e8921c620df570221a9f806ab2d3a44d81
+  //this gets a twist
+  useEffect(() => {
+    axios
+      .get("/api/twists/1")
+      .then((result) => {
+        console.log(result.data);
+        setData(result.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-useEffect(() => {
-  const fetchTwists = async () => {
-    const result = await axios.get('/api/data?search=Three Fish');
-    const body = JSON.parse(result.data.message);
-    setData(body);
-  };
-  fetchTwists();
-}, []);
+  //gets a recipe and sets recipe
+  useEffect(() => {
+    const fetchTwists = async () => {
+      //this variable needs to be hard coded to the search bar?
+      const result = await axios.get("/api/data?search=Three Fish");
+      const body = JSON.parse(result.data.message);
+      console.log(body);
+      setRecipe(body);
+    };
+    fetchTwists();
+  }, []);
 
-return (
-  <>
-    <ControlledCarousel />
-    <Container mw="100">
-      <Row>
-        <Col sm="6" >
-          <p>Fork It Favorites</p>
-          <h3>Name: {data.name}</h3>
-          <Image width="50%" src={data.meal_image} responsive />
-        </Col >
-        <Col sm="6" >
-          <p>Random Twists</p>
-          <Image height="75%" src="http://www.fillmurray.com/500/500" alt="" />
-        </Col>
-      </Row>
-    </Container>
-  </>
-);
-  }
+  return (
+    <>
+      <ControlledCarousel />
+      <Container mw="100">
+        <Row>
+          <Col sm="6">
+            <p>Fork It Favorites</p>
+            <h3>Name: {recipe.name}</h3>
+            <Image width="50%" src={recipe.meal_image} responsive />
+          </Col>
+          <Col sm="6">
+            <p>Random Twists</p>
+            <Image
+              height="75%"
+              src="http://www.fillmurray.com/500/500"
+              alt=""
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
