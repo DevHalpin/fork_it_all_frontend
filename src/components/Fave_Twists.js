cb3d3351bff97bd657357af6b7848991f907af6c
Kelsi <kelsiproulx1919@gmail.com>
2020-10-30 10:15:26 -0700

    add fave twists layout and individual id

--- src/components/Fave_Twists.js
+++ src/components/Fave_Twists.js
@@ -1,37 +1,40 @@
 import React, {useEffect, useState} from "react";
-import {Container, Card, CardGroup} from 'react-bootstrap';
+import {Card, CardDeck} from 'react-bootstrap';
 import '../styles/App.scss';
 import axios from 'axios';
 
 const Fave_Twists = () => {
-    
+
   const [twists, setTwists] = useState([]);
-  
+
   useEffect(() => {
     axios
-    .get(`/api/faveTwists`)
-    .then((response) => {
-      setTwists(response.data)
-    })
-  }, [])
-  console.log(twists)
+      .get(`/api/faveTwists`)
+      .then((response) => {
+        setTwists(response.data);
+      });
+  }, []);
+  console.log(twists);
+
+  const twistsCard = twists.map(twist => (
+    <Card key={twist.twist_id} style={{width: "30rem"}}>
+      <Card.Img src={`${twist.meal_image}`} variant="top" alt="Card image" />
+      <Card.Body>
+        <Card.Title>{`${twist.name}`}</Card.Title>
+        <Card.Text>
+          {`${twist.content}`}
+        </Card.Text>
+        <Card.Link href={`/recipes/${twist.recipe_id}/twists/${twist.twist_id}`} variant="primary">View this recipe</Card.Link>
+      </Card.Body>
+    </Card>
+  ));
+
   return (
-    <Container>
-      <CardGroup className="mt-3">
-        {twists.map(twist => (
-          <Card>
-            <Card.Img src={`${twist.meal_image}`} variant="top" alt="Card image" />
-            <Card.Body>
-              <Card.Title>{`${twist.name}`}</Card.Title>
-              <Card.Text>
-                {`${twist.content}`}
-              </Card.Text>
-              <Card.Link href={`/recipes/${twist.recipe_id}/twists/${twist.twist_id}`} variant="primary">View this recipe</Card.Link>
-            </Card.Body>
-          </Card>
-        ))}
-      </CardGroup>
-    </Container>
+    <>
+      <CardDeck className="mt-3">
+        {twistsCard}
+      </CardDeck>
+    </>
   );
 };
 
