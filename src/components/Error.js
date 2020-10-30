2b861b3c8aa2669ea7a6d059f75ec88965d92f05
Kelsi <kelsiproulx1919@gmail.com>
2020-10-29 20:26:01 -0700

    fix error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/App.scss';
+import {Alert} from "react-bootstrap";
 
 function Error() {
-  return (
-    <h1>Oops! Page not found!</h1>
-  );
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>
+          This page doesn't exist. Check again!
+        </p>
+      </Alert>
+    );
+  }
 }
 
 export default Error;
