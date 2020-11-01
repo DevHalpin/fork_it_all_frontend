d696e5d78172b0dc78f94c1a33597a9bb552626f
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-31 17:11:59 -0700

    fix twist content not moving after edit, create and delete. Create still needs to go to the :last route

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   CardDeck,
@@ -8,7 +8,12 @@ import {
   Form,
   Alert,
 } from "react-bootstrap";
-import {TwistCreateModal, TwistEditModal, TwistDeleteModal, TwistShareModal} from "./Modal";
+import {
+  TwistCreateModal,
+  TwistEditModal,
+  TwistDeleteModal,
+  TwistShareModal,
+} from "./Modal";
 import axios from "axios";
 import "../styles/Recipes.scss";
 import "../styles/App.scss";
@@ -39,8 +44,7 @@ const Recipes = (props) => {
   const checkFavorited = () => {
     if (favorites.includes(twist.id) && favorited === false) {
       setFavorited(true);
-    }
-    else if (!favorites.includes(twist.id) && favorited === true) {
+    } else if (!favorites.includes(twist.id) && favorited === true) {
       setFavorited(false);
     }
   };
@@ -48,12 +52,10 @@ const Recipes = (props) => {
   // Make a request for a recipe, random twist, and user given a recipe id
   useEffect(() => {
     if (twistId !== undefined) {
-      axios.get(`/api/recipes/${id}?twist=${twistId}`)
-        .then((response) => {
-          setTwist(response.data);
-        });
-    }
-    else {
+      axios.get(`/api/recipes/${id}?twist=${twistId}`).then((response) => {
+        setTwist(response.data);
+      });
+    } else {
       axios.get(`/api/recipes/${id}?random=1`).then((response) => {
         setTwist(response.data);
       });
@@ -61,9 +63,9 @@ const Recipes = (props) => {
     axios.get(`/api/recipes/${id}`).then((response) => {
       setRecipe(response.data);
     });
-    axios.get('/api/faveTwists').then((response) => {
+    axios.get("/api/faveTwists").then((response) => {
       const favoriteArr = [];
-      response.data.forEach(favorite => {
+      response.data.forEach((favorite) => {
         favoriteArr.push(favorite.twist_id);
       });
       setFavorites(favoriteArr);
@@ -77,6 +79,12 @@ const Recipes = (props) => {
   // Find a random twist
   const randomTwist = () => {
     axios.get(`/api/recipes/${id}?random=1`).then((response) => {
+      setTwist(response.data);
+    });
+  };
+  //used for updating the edit twist content
+  const specificTwist = (id, twist) => {
+    axios.get(`/api/recipes/${id}?twist=${twist}`).then((response) => {
       setTwist(response.data);
     });
   };
@@ -159,28 +167,40 @@ const Recipes = (props) => {
           onHide={handleCreateAlert}
           user={props.user}
           recipe={props.match.params.recipe}
+          random={() => {
+            randomTwist();
+          }}
         />
         <TwistEditModal
           show={isEditModalOpen}
           onHide={handleEditAlert}
           twist={twist ? twist : "no twist"}
+          getSpecifcTwist={() => {
+            specificTwist(id, twist.id);
+          }}
         />
         <TwistDeleteModal
           show={isDeleteModalOpen}
           onHide={toggleDeleteModal}
           twist={twist ? twist : undefined}
+          random={() => {
+            randomTwist();
+          }}
         />
-        {twist !== null ?
+        {twist !== null ? (
           <TwistShareModal
             show={isShareModalOpen}
             onHide={toggleShareModal}
             url={`http://localhost:3000/twists/${twist.slug}`}
           />
-          : null}
+        ) : null}
 
         {/* Show twists when disabled */}
         {showTwists === false ? (
-          <Button onClick={setShowTwists} className="gen-button login-buttons enable-button">
+          <Button
+            onClick={setShowTwists}
+            className="gen-button login-buttons enable-button"
+          >
             Enable Twists
           </Button>
         ) : null}
@@ -238,7 +258,10 @@ const Recipes = (props) => {
                   Rate
                 </Button>
               ) : null}
-              {userHandle && twist !== null && userHandle !== twist.handle && favorited === false ? (
+              {userHandle &&
+              twist !== null &&
+              userHandle !== twist.handle &&
+              favorited === false ? (
                 <Button
                   className="login-buttons gen-button"
                   bsPrefix
