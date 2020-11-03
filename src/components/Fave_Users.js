08d5d49fed311357fef3b269c70c2ec413967626
Kelsi Proulx <Kelsiproulx1919@gmail.com>
2020-11-03 08:14:57 -0800

    Add fave users to production

--- src/components/Fave_Users.js
+++ src/components/Fave_Users.js
@@ -1,55 +1,124 @@
-import React, {useState, useEffect} from "react";
-import {Container, Card, CardDeck, Row} from 'react-bootstrap';
-import '../styles/App.scss';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Container, Card, CardDeck, Row } from "react-bootstrap";
+import "../styles/App.scss";
+import axios from "axios";
 
 const Fave_Users = (props) => {
   const id = props.match.params.user;
-  const fakeUser = {
+  const fakeUser1 = {
     id: 101,
     email: "charla@oconnell.com",
-    profile_picture: "https://www.fillmurray.com/200/300",
+    profile_picture:
+      "https://www.lighthouselabs.ca/uploads/team_member/avatar/212/medium_IMG-20180109-WA0021.jpg",
     handle: "evia.pouros",
     created_at: "2020-10-26T23:07:26.000Z",
     updated_at: "2020-10-26T23:07:26.000Z",
-    name: "Everett Predovic",
-    bio:
-      "Labore et excepturi vero expedita aut reiciendis laboriosam inventore fugiat voluptatum a ipsum itaque sapiente accusamus amet doloribus aliquid necessitatibus laborum quasi ea illo numquam temporibus aperiam voluptatem aut aut odit repellat ut occaecati quo id vero est asperiores natus fuga et architecto consequuntur atque est nobis eum sed officia nemo voluptatem ullam quas ut veniam vitae iusto et sequi quis ut sint nulla asperiores cumque similique veritatis in quia a velit sunt nihil suscipit sit voluptate iure placeat natus doloribus tempora voluptate et eum suscipit saepe error deserunt velit ipsa consequatur in accusantium laudantium non consequatur eveniet et provident delectus esse qui aspernatur nam tempore eligendi exercitationem iste eius ea nisi qui consequatur quis excepturi sapiente corporis reprehenderit voluptatum omnis eos hic voluptatem quia blanditiis recusandae quidem ut quo dicta doloremque sit dolor qui quia est repellat nostrum tenetur cupiditate aspernatur et impedit assumenda necessitatibus cupiditate id assumenda quos magnam et sed dolores et reprehenderit tempore dolores impedit dolorum illum omnis ea iste libero cum hic perferendis rem veniam voluptatem culpa deleniti placeat quos ea minus facilis enim labore est deserunt itaque maiores quas consequatur et non adipisci nesciunt aut quaerat voluptatem qui rem ipsam esse numquam magni ab ipsum et nesciunt quae sed exercitationem dignissimos est minus debitis dolor fugiat dolores ut culpa id voluptates libero vel aut maxime aperiam error molestiae voluptas soluta perspiciatis quibusdam eum perspiciatis quaerat qui praesentium magnam atque autem est eius est dolorem laudantium odio nihil tenetur distinctio neque recusandae molestias amet earum porro quisquam beatae sunt alias dicta expedita non unde perferendis rerum saepe facilis dolorem maiores sit et elorem consequatur ut repudiandae qui cum provident fugit minima totam optio occaecati ab omnis sit consectetur quis aliquid vel vel quia modi rerum ut sed laborum aut eaque tempora et cumque ad qui iure doloremque distinctio voluptatem incidunt animi rerum illum molestiae non eos et ut est quo earum voluptatem eligendi velit dolor non facere ratione enim quae consequuntur nobis rerum dignissimos sit molestiae ducimus iusto harum molestias nihil debitis corrupti quia sed nulla?",
+    name: "Hafiz Suara",
   };
 
-  const [user, setUser] = useState(fakeUser);
+  const fakeUser2 = {
+    email: "charla@oconnell.com",
+    profile_picture:
+      "https://www.lighthouselabs.ca/uploads/team_member/avatar/371/medium_Headshot.png",
+    handle: "evia.pouros",
+    created_at: "2020-10-26T23:07:26.000Z",
+    updated_at: "2020-10-26T23:07:26.000Z",
+    name: "Travis Borsa",
+  };
+
+  const fakeUser3 = {
+    email: "charla@oconnell.com",
+    profile_picture:
+      "https://www.lighthouselabs.ca/uploads/team_member/avatar/330/medium_Andy_Lindsay_profile.jpeg",
+    handle: "evia.pouros",
+    created_at: "2020-10-26T23:07:26.000Z",
+    updated_at: "2020-10-26T23:07:26.000Z",
+    name: "Andy Lindsay",
+  };
+
+  const fakeUser4 = {
+    email: "charla@oconnell.com",
+    profile_picture:
+      "https://www.lighthouselabs.ca/uploads/team_member/avatar/219/medium_Screen_Shot_2018-02-01_at_6.08.30_PM.png",
+    handle: "evia.pouros",
+    created_at: "2020-10-26T23:07:26.000Z",
+    updated_at: "2020-10-26T23:07:26.000Z",
+    name: "Caroline Lauder",
+  };
+
+  const fakeUser5 = {
+    email: "charla@oconnell.com",
+    profile_picture:
+      "https://s3.amazonaws.com/lighthouse-compass2/uploads/avatar/custom_avatar/4215/thumb_70672238_10215144203261500_2965875784918499328_o.jpg",
+    handle: "evia.pouros",
+    created_at: "2020-10-26T23:07:26.000Z",
+    updated_at: "2020-10-26T23:07:26.000Z",
+    name: "Bradley Fung",
+  };
+
+  const fakeUser6 = {
+    email: "charla@oconnell.com",
+    profile_picture:
+      "https://pbs.twimg.com/profile_images/588361371563102210/LrmTN4Io.jpg",
+    handle: "evia.pouros",
+    created_at: "2020-10-26T23:07:26.000Z",
+    updated_at: "2020-10-26T23:07:26.000Z",
+    name: "Khurram Virani",
+  };
+
+  const fakeUser7 = {
+    email: "charla@oconnell.com",
+    profile_picture:
+      "https://www.lighthouselabs.ca/uploads/team_member/avatar/378/medium_20190819_091501.jpg",
+    handle: "evia.pouros",
+    created_at: "2020-10-26T23:07:26.000Z",
+    updated_at: "2020-10-26T23:07:26.000Z",
+    name: "Kelly Gordon",
+  };
+
+  const fakeUser8 = {
+    email: "charla@oconnell.com",
+    profile_picture:
+      "https://www.lighthouselabs.ca/uploads/team_member/avatar/396/medium_120-0-4.jpg",
+    handle: "evia.pouros",
+    created_at: "2020-10-26T23:07:26.000Z",
+    updated_at: "2020-10-26T23:07:26.000Z",
+    name: "Meri Ghazaryan",
+  };
+
+  // const [user, setUser] = useState(fakeUser);
   // Make a request for a user with a given ID
-  useEffect(() => {
-    axios
-      .get(`https://stark-shelf-20245.herokuapp.com/api/users/${id}`)
-      .then((response) => {
-        setUser(response.data);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }, [id]);
+  // useEffect(() => {
+  //   axios
+  //     .get(`/api/users/${id}`)
+  //     .then((response) => {
+  //       setUser(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }, [id]);
 
   return (
     <Container>
-      <Row style={{flex: 1}}>
+      <Row style={{ flex: 1 }}>
         <CardDeck className="mt-3">
           <Card>
-            <Card.Img variant="top" src={`${user.profile_picture}`} />
+            <Card.Img variant="top" src={`${fakeUser1.profile_picture}`} />
             <Card.Body>
-              <Card.Title>{`${user.name}`}</Card.Title>
+              <Card.Title>{`${fakeUser1.name}`}</Card.Title>
             </Card.Body>
           </Card>
           <Card>
-            <Card.Img variant="top" src={`${user.profile_picture}`} />
+            <Card.Img variant="top" src={`${fakeUser2.profile_picture}`} />
             <Card.Body>
-              <Card.Title>{`${user.name}`}</Card.Title>
+              <Card.Title>{`${fakeUser2.name}`}</Card.Title>
             </Card.Body>
           </Card>
           <Card>
-            <Card.Img variant="top" src={`${user.profile_picture}`} />
+            <Card.Img variant="top" src={`${fakeUser3.profile_picture}`} />
             <Card.Body>
-              <Card.Title>{`${user.name}`}</Card.Title>
+              <Card.Title>{`${fakeUser3.name}`}</Card.Title>
             </Card.Body>
           </Card>
         </CardDeck>
@@ -57,21 +126,33 @@ const Fave_Users = (props) => {
       <Row>
         <CardDeck className="mt-3">
           <Card>
-            <Card.Img variant="top" src={`${user.profile_picture}`} />
+            <Card.Img variant="top" src={`${fakeUser4.profile_picture}`} />
             <Card.Body>
-              <Card.Title>{`${user.name}`}</Card.Title>
+              <Card.Title>{`${fakeUser4.name}`}</Card.Title>
             </Card.Body>
           </Card>
           <Card>
-            <Card.Img variant="top" src={`${user.profile_picture}`} />
+            <Card.Img variant="top" src={`${fakeUser5.profile_picture}`} />
             <Card.Body>
-              <Card.Title>{`${user.name}`}</Card.Title>
+              <Card.Title>{`${fakeUser5.name}`}</Card.Title>
             </Card.Body>
           </Card>
           <Card>
-            <Card.Img variant="top" src={`${user.profile_picture}`} />
+            <Card.Img variant="top" src={`${fakeUser6.profile_picture}`} />
             <Card.Body>
-              <Card.Title>{`${user.name}`}</Card.Title>
+              <Card.Title>{`${fakeUser6.name}`}</Card.Title>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={`${fakeUser7.profile_picture}`} />
+            <Card.Body>
+              <Card.Title>{`${fakeUser7.name}`}</Card.Title>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={`${fakeUser8.profile_picture}`} />
+            <Card.Body>
+              <Card.Title>{`${fakeUser8.name}`}</Card.Title>
             </Card.Body>
           </Card>
         </CardDeck>
