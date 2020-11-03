3937b11ea545d79e3c6fdfa88a03eb78fd789e17
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-11-03 10:23:34 -0800

    auth token now working on front end albeit happy path

--- src/components/My_Twists.js
+++ src/components/My_Twists.js
@@ -1,42 +1,47 @@
-import React, {useEffect, useState} from "react";
-import {Container, Card, Row} from 'react-bootstrap';
-import '../styles/App.scss';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Container, Card, Row } from "react-bootstrap";
+import "../styles/App.scss";
+import axios from "axios";
 
-const My_Twists = () => {
-
+const My_Twists = (props) => {
   const [twists, setTwists] = useState([]);
-
+  const user = props.user;
+  console.log(user);
   // Make a request for a recipe, random twist, and user given a recipe id
   useEffect(() => {
     axios
-      .get(`/api/myTwists`)
+      .get(`/api/myTwists`, {
+        headers: {
+          authorization: `Token token=${user.access_token}`,
+        },
+      })
       .then((response) => {
         setTwists(response.data);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }, []);
 
-  const twistsCard = twists.map(twist => (
+  const twistsCard = twists.map((twist) => (
     <Card key={twist.twist_id} className="user-saved">
       <Card.Img src={`${twist.meal_image}`} variant="top" alt="Card image" />
       <Card.Body>
         <Card.Title>{`${twist.name}`}</Card.Title>
-        <Card.Text>
-          {`${twist.content}`}
-        </Card.Text>
-        <Card.Link href={`/recipes/${twist.recipe_id}/twists/${twist.twist_id}`} className="card-link">View this recipe</Card.Link>
+        <Card.Text>{`${twist.content}`}</Card.Text>
+        <Card.Link
+          href={`/recipes/${twist.recipe_id}/twists/${twist.twist_id}`}
+          className="card-link"
+        >
+          View this recipe
+        </Card.Link>
       </Card.Body>
     </Card>
   ));
 
   return (
     <Container className="d-flex">
-      <Row className="justify-content-center">
-        {twistsCard}
-      </Row>
+      <Row className="justify-content-center">{twistsCard}</Row>
     </Container>
   );
 };
